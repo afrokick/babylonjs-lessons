866ed382189af7b5009fed92ad81129b6fe0f65b
afrokick <devbyru@gmail.com>
2023-01-24 16:40:50 +0300

    format code fix ESModule path to Legacy/legacy

--- babylonjs-from-zero/src/main.js
+++ babylonjs-from-zero/src/main.js
@@ -1,4 +1,4 @@
-import * as BABYLON from "https://esm.sh/@babylonjs/core@5.43.1";
+import * as BABYLON from "https://esm.sh/@babylonjs/core@5.43.1/Legacy/legacy";
 
 const canvas = document.querySelector("#game-canvas");
 const engine = new BABYLON.Engine(canvas, true, { stencil: true }, true);
@@ -7,29 +7,38 @@ const createScene = function () {
   // Creates a basic Babylon Scene object
   const scene = new BABYLON.Scene(engine);
   // Creates and positions a free camera
-  const camera = new BABYLON.FreeCamera("camera1", 
-      new BABYLON.Vector3(0, 5, -10), 
-      scene);
+  const camera = new BABYLON.FreeCamera(
+    "camera1",
+    new BABYLON.Vector3(0, 5, -10),
+    scene
+  );
   // Targets the camera to scene origin
   camera.setTarget(BABYLON.Vector3.Zero());
   // Attaches the camera to the canvas
   camera.attachControl(canvas, true);
   // Creates a light, aiming 0,1,0
-  const light = new BABYLON.HemisphericLight("light", 
-      new BABYLON.Vector3(0, 1, 0), 
-      scene);
+  const light = new BABYLON.HemisphericLight(
+    "light",
+    new BABYLON.Vector3(0, 1, 0),
+    scene
+  );
   // Dim the light a small amount 0 - 1
   light.intensity = 0.7;
   // Built-in 'sphere' shape.
-  const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", 
-      {diameter: 2, segments: 32}, 
-      scene);
+  const sphere = BABYLON.MeshBuilder.CreateSphere(
+    "sphere",
+    { diameter: 2, segments: 32 },
+    scene
+  );
   // Move phere upward 1/2 its height
   sphere.position.y = 1;
   // Built-in 'ground' shape.
-  const ground = BABYLON.MeshBuilder.CreateGround("ground", 
-      {width: 6, height: 6}, 
-      scene);
+  const ground = BABYLON.MeshBuilder.CreateGround(
+    "ground",
+    { width: 6, height: 6 },
+    scene
+  );
+
   return scene;
 };
 
--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-declare module "https://esm.sh/@babylonjs/core@5.43.1" { 
+declare module "https://esm.sh/@babylonjs/core@5.43.1/Legacy/legacy" { 
   export * from "@babylonjs/core"
 }
